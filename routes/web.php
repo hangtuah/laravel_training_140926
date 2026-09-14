589a84f5edf61033a478fb2aa942a184fcf47c5b
Tc. Mohd Fairus bin Ahmad <mfairusahmad@utm.my>
2026-09-14 15:18:57 +0800

    feat: add route for booking and room

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//ROOMS
+Route::get('/rooms', [App\Http\Controllers\RoomController::class, 'index'])
+    ->name('rooms.index');
+
+Route::get('/rooms/add', [App\Http\Controllers\RoomController::class, 'add'])
+    ->name('rooms.add');
+
+Route::post('/rooms/store', [App\Http\Controllers\RoomController::class, 'store'])
+    ->name('rooms.store');
+
+Route::get('/rooms/edit/{room_id}', [App\Http\Controllers\RoomController::class, 'edit'])
+    ->name('rooms.edit');
+
+Route::put('/rooms/update/{room_id}', [App\Http\Controllers\RoomController::class, 'update'])
+    ->name('rooms.update');
+
+Route::delete('/rooms/delete/{room_id}', [App\Http\Controllers\RoomController::class, 'destroy'])
+    ->name('rooms.destroy');
+
+//BOOKING
+Route::get('/bookings', [App\Http\Controllers\BookingController::class, 'index'])
+    ->name('bookings.index');
+
+Route::get('/bookings/add', [App\Http\Controllers\BookingController::class, 'add'])
+    ->name('bookings.add');
+
+Route::post('/bookings/store', [App\Http\Controllers\BookingController::class, 'store'])
+    ->name('bookings.store');
+
+Route::get('/bookings/edit/{booking_id}', [App\Http\Controllers\BookingController::class, 'edit'])
+    ->name('bookings.edit');
+
+Route::put('/bookings/update/{booking_id}', [App\Http\Controllers\BookingController::class, 'update'])
+    ->name('bookings.update');
+
+Route::delete('/bookings/delete/{booking_id}', [App\Http\Controllers\BookingController::class, 'destroy'])
+    ->name('bookings.destroy');
