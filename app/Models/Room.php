d4caffe357fde229290fc21c5b422d139390ce71
Tc. Mohd Fairus bin Ahmad <mfairusahmad@utm.my>
2026-09-14 15:07:41 +0800

    feat: define Room hasMany and Booking belongsTo relationships

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class Room extends Model
 {
-    //
+    protected $table = 'rooms';
+
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
